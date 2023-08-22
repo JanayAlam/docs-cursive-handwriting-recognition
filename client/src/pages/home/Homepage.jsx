7fd4004f6ebae581d31708af147d5865e7f8b141
JanayAlam <dev.janay@outlook.com>
2023-08-22 15:36:36 +0600

    feat: Added get medicines route and retouch homepage UI

--- client/src/pages/home/Homepage.jsx
+++ client/src/pages/home/Homepage.jsx
@@ -1,3 +1,4 @@
+import { Grid } from '@mui/material';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
@@ -41,29 +42,70 @@ const Homepage = () => {
     };
 
     return (
-        <Box className={styles.homepageRoot}>
-            <Container maxWidth="xl">
-                <Box className={styles.homepageInner}>
-                    <Typography
-                        variant="h2"
-                        component="h2"
-                        className={styles.header}
-                        sx={{ fontWeight: 'bold' }}
-                    >
-                        Trust Our Experience
-                    </Typography>
-                    <Typography
-                        className={styles.caption}
-                        sx={{ fontSize: '1.2rem' }}
-                    >
-                        Get started by uploading prescriptions
-                    </Typography>
-                    <Box sx={{ mt: '1rem' }}>
-                        <Dropzone handleSelect={handleImageSelect} />
+        <>
+            <Box className={styles.homepageRoot}>
+                <Container maxWidth="xl">
+                    <Box className={styles.homepageInner}>
+                        <Typography
+                            variant="h2"
+                            component="h2"
+                            className={styles.header}
+                            sx={{ fontWeight: 'bold' }}
+                        >
+                            Trust Our Experience
+                        </Typography>
+                        <Typography
+                            className={styles.caption}
+                            sx={{ fontSize: '1.2rem' }}
+                        >
+                            Get started by uploading prescriptions
+                        </Typography>
+                        <Box sx={{ mt: '1rem' }}>
+                            <Dropzone handleSelect={handleImageSelect} />
+                        </Box>
                     </Box>
-                </Box>
-            </Container>
-        </Box>
+                </Container>
+            </Box>
+            <div className={styles.footer}>
+                <Container maxWidth="xl">
+                    <Grid container spacing={2}>
+                        <Grid item md={6}>
+                            <p>
+                                Research:{' '}
+                                <b>
+                                    Doctors' cursive handwritten prescription
+                                    recognition and alternative brands
+                                    suggestion
+                                </b>
+                            </p>
+                            <p>
+                                Supervisor: <b>Tanni Mittra</b>, Senior Lecturer
+                            </p>
+                            <p>
+                                Authors: <b>Md. Janay Alam</b>,{' '}
+                                <b>Najmus Sakib</b>, <b>Afsana Nur Meem</b>,{' '}
+                                <b>Lamya Ishrat Nodi</b>, and{' '}
+                                <b>Efte Kharul Islam Shinha</b>
+                            </p>
+                            <Typography variant="body2">
+                                Department of Computer Science & Engineering
+                                <br />
+                                <b>East West University</b>, Dhaka, Bangladesh
+                            </Typography>
+                        </Grid>
+                        <Grid item md={6} sx={{ textAlign: 'right' }}>
+                            <p>
+                                Technology Used: <b>React JS</b>, <b>Flask</b>,{' '}
+                                Tensorflow, Open CV, etc.
+                            </p>
+                            <p>
+                                UI: <b>Material UI</b>
+                            </p>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </div>
+        </>
     );
 };
 
